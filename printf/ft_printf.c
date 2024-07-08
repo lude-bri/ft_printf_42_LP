a3c020d2c453bf32f824abd2adc561d1d8750867
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-07-08 08:59:15 +0100

    printf_update

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -60,29 +60,3 @@ static int	f_specifiers(char str, va_list a)
 		count += ft_putchar('%');
 	return (count);
 }
-int	main(void)
-{
-	// char c = 'k'; // %c
-	// char *str = "String test"; // %s
-	// char *var; var = &c;// %p
-	// int	nbr = -5;// %d // %i
-	// unsigned int nbrr = 20;// %u
-	// int b= 0xffffffff;// %x // %X
-	// // %%
-	// int i;
-	// printf("Printf replica:\n");
-	// i = ft_printf("| TEST TEXT |\n| Single Char: %c |\n| String: %s |\n| \
-	// 	Pointer Adress: %p |\n| Decimal: %d |\n| Integer %i |\n| \
-	// 	Unsign deci: %u |\n| HexLower: %x |\n| HexUpper: %X |\n| \
-	// 	Percentage sign: %% |\n", c, str, var, nbr, nbr, nbrr, b, b);
-	// printf("\nReturn Value: %d", i);
-	// ///////////////////////////////
-	// printf("\nPrintf original:\n");
-	// i = printf("| TEST TEXT |\n| Single Char: %c |\n| String: %s |\n| \
-	// 	Pointer Adress: %p |\n| Decimal: %d |\n| Integer %i |\n| \
-	// 	Unsign deci: %u |\n| HexLower: %x |\n| HexUpper: %X |\n| \
-	// 	Percentage sign: %% |\n", c, str, var, nbr, nbr, nbrr, b, b);
-	// printf("\nReturn Value: %d", i);
-	// return (0);
-
-}
